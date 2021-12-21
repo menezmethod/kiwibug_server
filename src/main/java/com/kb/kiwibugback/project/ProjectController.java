2ee6421df49cd705a4b4c167b8300635034c3dc5
MenezMethod <menezfd@gmail.com>
2021-12-21 16:48:28 -0700

    fixed an accidental comment that totally broke put mapping for project

--- src/main/java/com/kb/kiwibugback/project/ProjectController.java
+++ src/main/java/com/kb/kiwibugback/project/ProjectController.java
@@ -48,23 +48,23 @@ public class ProjectController {
                 }
         }
 
-//        @PutMapping("{id}")
-////        @PreAuthorize("hasRole('USER') or hasRole('LEAD') or hasRole('MANAGER') or hasRole('ADMIN')")
-//
-//        public ResponseEntity<Project> updateProject(@PathVariable("id") long id, @RequestBody Project project) {
-//                Optional<Project> projectData = projectRepository.findById(id);
-//
-//                if (projectData.isPresent()) {
-//                        Project _project = projectData.get();
-//                        _project.setProjectName(project.getProjectName());
-//                        _project.setStartDate(project.getStartDate());
-//                        _project.setTargetEndDate(project.getTargetEndDate());
-//                        _project.setActualEndDate(project.getActualEndDate());
-//                        return new ResponseEntity<>(projectRepository.save(_project), HttpStatus.OK);
-//                } else {
-//                        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//                }
-//        }
+        @PutMapping("{id}")
+//        @PreAuthorize("hasRole('USER') or hasRole('LEAD') or hasRole('MANAGER') or hasRole('ADMIN')")
+
+        public ResponseEntity<Project> updateProject(@PathVariable("id") long id, @RequestBody Project project) {
+                Optional<Project> projectData = projectRepository.findById(id);
+
+                if (projectData.isPresent()) {
+                        Project _project = projectData.get();
+                        _project.setProjectName(project.getProjectName());
+                        _project.setStartDate(project.getStartDate());
+                        _project.setTargetEndDate(project.getTargetEndDate());
+                        _project.setActualEndDate(project.getActualEndDate());
+                        return new ResponseEntity<>(projectRepository.save(_project), HttpStatus.OK);
+                } else {
+                        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+        }
 
         @DeleteMapping("{id}")
 //        @PreAuthorize("hasRole('USER') or hasRole('LEAD') or hasRole('MANAGER') or hasRole('ADMIN')")
